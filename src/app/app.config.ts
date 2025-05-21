94e450bb6fa2c6ccab48be48d04ea6827611e882
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2025-05-21 20:06:18 -0300

    Chore | Setting up firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTu0HD3k0jtaoGLEMBhgVp4roJGGZDAn0",
+  authDomain: "ibebb-2ce3f.firebaseapp.com",
+  projectId: "ibebb-2ce3f",
+  storageBucket: "ibebb-2ce3f.firebasestorage.app",
+  messagingSenderId: "351201144515",
+  appId: "1:351201144515:web:a0d3bfc9538549da257c76"
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ]
 };
