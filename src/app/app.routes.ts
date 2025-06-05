4a95bc0da46b0b0fb1bb580f58e9f4a438264e74
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2025-06-05 00:55:15 -0300

    Chore | Adding app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,24 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { 
+    path: 'home', 
+    loadComponent: () => import('./features/home/components/home.component').then(m => m.HomeComponent),
+    title: 'IBE'
+  },
+  { 
+    path: 'novidades-e-eventos', 
+    loadComponent: () => import('./features/news-and-events/components/news-and-events.component').then(m => m.NewsAndEventsComponent),
+    title: 'Novidades e Eventos | IBE'
+  },
+  { 
+    path: 'fale-conosco', 
+    loadComponent: () => import('./features/contact-us/components/contact-us.component').then(m => m.ContactUsComponent),
+    title: 'Fale Conosco | IBE'
+  },
+  { 
+    path: '**', 
+    loadComponent: () => import('./features/home/components/home.component').then(m => m.HomeComponent) 
+  }
 ];
