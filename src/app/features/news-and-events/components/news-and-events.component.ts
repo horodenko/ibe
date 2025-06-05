9048874978b9d35c8ce4d05bd78536d6cddd1abf
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2025-06-05 00:53:05 -0300

    Feat | NewsAndEvents - Creating component

---
+++ src/app/features/news-and-events/components/news-and-events.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-and-events',
+  imports: [],
+  templateUrl: './news-and-events.component.html',
+  styleUrl: './news-and-events.component.scss'
+})
+export class NewsAndEventsComponent {
+
+}
